c111410b0d11f220d0e94ce15bcecb76e1a217ad
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-01-26 17:11:13 +0200

    first commit

---
+++ src/data/Products.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    _id: "1",
+    name: "Under Armour Men's HeatGear Compression Long-Sleeve T-Shirt",
+    image: "/images/1.jpeg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 30,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    _id: "2",
+    name: "Disney Men's Full Size Mickey Mouse Distressed Look T-Shirt",
+    image: "/images/2.jpeg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 25,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 2,
+  },
+  {
+    _id: "3",
+    name: "Under Armour Women's Tech V-Neck Twist Short-Sleeve T-Shirt",
+    image: "/images/3.jpeg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 22,
+    countInStock: 0,
+    rating: 4.5,
+    numReviews: 3,
+  },
+  {
+    _id: "4",
+    name: "HUK Men's Short Sleeve Tee | Performance Fishing T-Shirt",
+    image: "/images/4.jpeg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 20,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    _id: "5",
+    name: "Salt Life Men's Skull and Hooks Short Sleeve Pocket Crew Neck Tee",
+    image: "/images/5.jpeg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 22,
+    countInStock: 7,
+    rating: 4,
+    numReviews: 2,
+  },
+  {
+    _id: "6",
+    name: "Life is Good Women's Crusher Graphic Long Sleeve T-Shirt Three Daisies",
+    image: "/images/6.jpeg",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 32,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+
+export default products;
